b880800dc8fee52e71eceed1fe9133407b0a2516
missmailan2012 <missmailan2012@gmail.com>
2014-01-28 22:26:42 +0700

    Success!

---
+++ Generation CharCode/Generation CharCode/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Generation CharCode
+//
+//  Created by Dao Duy Nhan on 25/01/2014.
+//  Copyright (c) Năm 2014 Dao Duy Nhan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, lozWorld!\n");
+    return 0;
+}
+
