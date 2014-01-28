84bb8a022c32f0aecbb247203a777a51a90154e3
missmailan2012 <missmailan2012@gmail.com>
2014-01-28 21:43:58 +0700

    Hello!

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Dao Duy Nhan on 28/01/2014.
+//  Copyright (c) Năm 2014 Dao Duy Nhan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Xin chào các bạn!\n");
+    return 0;
+}
+
